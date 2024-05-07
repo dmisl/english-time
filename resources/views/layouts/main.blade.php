e7596b993e263ec28650c2bbf497fbc05272020e
dmisl <rithlesscorp@gmail.com>
2024-05-08 00:01:12 +0200

    favicon for all pages

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1875,7 +1875,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     {{-- CKEDITOR --}}
     <script src="https://cdn.ckeditor.com/ckeditor5/40.1.0/classic/ckeditor.js"></script>
-
+    <link rel="icon" type="image/x-icon" href="{{ asset('storage/icons/eng.png') }}">
 </head>
 
 <body>
